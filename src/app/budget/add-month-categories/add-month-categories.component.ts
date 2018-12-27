a47281786e0ed062f548b0905a168b467bc41cc9
tetty <t.lebed17@gmail.com>
2018-12-27 19:07:59 +0200

    add-month-categories component

---
+++ src/app/budget/add-month-categories/add-month-categories.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {MonthCategoryService} from '../../services/month-category.service';
+import {CategoryModel} from '../../Models/CategoryModel';
+
+@Component({
+  selector: 'app-add-month-categories',
+  templateUrl: './add-month-categories.component.html',
+  styleUrls: ['./add-month-categories.component.scss']
+})
+export class AddMonthCategoriesComponent implements OnInit {
+  categories: CategoryModel[];
+
+  constructor(private service: MonthCategoryService) { }
+
+  ngOnInit() {
+    this.service.getAvailable().subscribe((data: CategoryModel[]) => this.categories = data);
+  }
+
+}
